23397265842cc24e73c69e6ead508565d78ef064
Ernesto De Los Santos <netos_id@yahoo.com>
2016-06-14 16:07:00 -0500

    Working on the framework. Working tohave text on image.

--- build.js
+++ build.js
@@ -3,14 +3,14 @@ import { render } from 'react-dom'
 import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import App from './modules/App'
 import Home from './modules/Home'
-import Instructors from './modules/Instructors'
+import About from './modules/About'
 
 
 render((
   <Router history={browserHistory}>
     <Route path="/" component={App}>
       <Route path="/home" component={Home}/>
-      <Route path="/instructors" component={Instructors}/>
+      <Route path="/about" component={About}/>
     </Route>
   </Router>
 ), document.getElementById('app'))
--- modules/App.js
+++ modules/App.js
@@ -1,17 +1,30 @@
 import React from 'react'
 import { Link } from 'react-router'
 
+
 export default React.createClass({
   render() {
     return (
       <div>
         <h1 className="heading">Jiu-Jits App!</h1>
         <ul role="nav">
-          <li><Link to="/home">Home</Link></li>
-          <li><Link to="/instructors">Instructors</Link></li>
+          <li className="nav__top"><Link to="/home">Home</Link></li>
+          <li className="nav__top"><Link to="/about">About</Link></li>
         </ul>
-          <a href={'http://rodrigopinheirobjj.com/wp-content/uploads/2015/06/rp_bjj_academy_01.jpg'} />
-          <img src={'http://www.teamsharkbjj.com/wp-content/uploads/2015/12/12341098_857432434354480_1637971756259014799_n.jpg'} />
+        <section className="container">
+            <li>
+              <div className="image__text">
+                <h2>Click</h2>
+                <img className="school" type="image" src='../modules/images/rpbjj-logo.jpg' />
+              </div>
+            </li>
+            <li>
+              <div className="image__text">
+                <h2>Click</h2>
+                  <img className="shark" type="image" src='../modules/images/teamshark-logo.jpg' />
+              </div>
+            </li>
+        </section>
 
         {this.props.children}
 
